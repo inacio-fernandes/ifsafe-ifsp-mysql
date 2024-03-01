c686f3f4a88d7930ae8da6c77530efbb0241eeb4
inacio-fernandes <inaciofsantana3101@gmail.com>
2024-02-29 22:07:41 -0300

    2 certos

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-// Rota para retornar um JSON
-app.get("/dados", (req, res) => {
-  const dados = [
-    { id: 1, nome: "Alice", idade: 25 },
-    { id: 2, nome: "Bob", idade: 30 },
-    { id: 3, nome: "Charlie", idade: 22 },
-    // Adicione mais dados conforme necessário
-  ];
-
-  res.json(dados);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log("Servidor escutando na porta 3000");
 });
