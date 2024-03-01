ff5b8f156897f7fdeb55f0360a4469197dfe5a08
inacio-fernandes <76115803+inacio-fernandes@users.noreply.github.com>
2024-02-29 23:37:50 -0300

    Add files via upload
    agora tudo

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => {
-  console.log("Servidor escutando na porta 3000");
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
